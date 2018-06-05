4f58f5f60154336e4c1bfd64982d41a41e3228ac
FazioNico <contact@nicolasfazio.ch>
2018-06-05 10:27:41 +0200

    step03

--- MyApp/src/app/app.routing.ts
+++ MyApp/src/app/app.routing.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
   {
-    path: 'index',
+    path: '',
     loadChildren: './features/index-page/index-page.module#IndexPageModule'
   },
 ];
