fe001f154eb9261d7c8bdfc33b0aaf48d18889a4
FazioNico <contact@nicolasfazio.ch>
2018-06-07 12:30:43 +0200

    add toast

--- MyApp/src/app/features/tab-one/containers/tab-one/tab-one.component.ts
+++ MyApp/src/app/features/tab-one/containers/tab-one/tab-one.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Capacitor, Plugins } from '@capacitor/core';
+import { Capacitor, Plugins, GeolocationPosition } from '@capacitor/core';
+import { Observable, of, from as fromPromise} from 'rxjs';
+
+const { Toast, Geolocation } = Capacitor.Plugins;
 
 @Component({
   selector: 'app-tab-one',
@@ -8,22 +11,25 @@ import { Capacitor, Plugins } from '@capacitor/core';
 })
 export class TabOneComponent implements OnInit {
 
+  public coordinates$: Observable<GeolocationPosition>;
   constructor() { }
 
   ngOnInit() {
     this.getCurrentPosition();
   }
-  async getCurrentPosition() {
-    const ready = Capacitor.isPluginAvailable('Geolocation');
-    if (!ready) {
-      return console.log('Error Geolocation not available');
+
+  async getCurrentPosition(): Promise<Observable<GeolocationPosition|Error>> {
+    const isAvailable: boolean = Capacitor.isPluginAvailable('Geolocation');
+    if (!isAvailable) {
+      console.log('Err: plugin not available');
+      return of(new Error('Err: plugin not available'));
     }
-    const { Geolocation } = Plugins;
-    const coordinates = await Geolocation.getCurrentPosition();
-    if (coordinates instanceof Error) {
-      return console.log('Error position', coordinates.message);
-    }
-    console.log('Current', coordinates);
+    return this.coordinates$ = fromPromise(Geolocation.getCurrentPosition());
   }
 
+  async show() {
+    await Toast.show({
+      text: 'Hello!'
+    });
+  }
 }
