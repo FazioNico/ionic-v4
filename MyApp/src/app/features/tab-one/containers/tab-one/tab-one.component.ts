0430f36f4a3046b887cd758b11837681dd46293e
FazioNico <contact@nicolasfazio.ch>
2018-06-07 12:24:49 +0200

    native cap

--- MyApp/src/app/features/tab-one/containers/tab-one/tab-one.component.ts
+++ MyApp/src/app/features/tab-one/containers/tab-one/tab-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Capacitor, Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab-one',
@@ -10,6 +11,19 @@ export class TabOneComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getCurrentPosition();
+  }
+  async getCurrentPosition() {
+    const ready = Capacitor.isPluginAvailable('Geolocation');
+    if (!ready) {
+      return console.log('Error Geolocation not available');
+    }
+    const { Geolocation } = Plugins;
+    const coordinates = await Geolocation.getCurrentPosition();
+    if (coordinates instanceof Error) {
+      return console.log('Error position', coordinates.message);
+    }
+    console.log('Current', coordinates);
   }
 
 }
