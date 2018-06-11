a0f896c305e5227f9166d790209517abb6c6a464
FazioNico <contact@nicolasfazio.ch>
2018-06-11 09:22:47 +0200

    step motion plugin

--- MyApp/src/app/features/tab-three/containers/tab-three/tab-three.component.ts
+++ MyApp/src/app/features/tab-three/containers/tab-three/tab-three.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Capacitor, Plugins, GeolocationPosition } from '@capacitor/core';
+import { of, Observable } from 'rxjs';
+
+const { Motion } = Capacitor.Plugins;
 
 @Component({
   selector: 'app-tab-three',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabThreeComponent implements OnInit {
 
+  public motionDatas: Observable<any> = of({message: 'loading...'});
+
   constructor() { }
 
   ngOnInit() {
+    this.startMotion();
   }
 
+  startMotion() {
+    const ready = Capacitor.isPluginAvailable('Motion');
+    if (!ready) {
+      this.motionDatas = of({message: 'error Capacitaor Motion not available'});
+      return this.handlError();
+    }
+    Motion.addListener('orientation', (data) => {
+      console.log('::::: data motion', data);
+      this.motionDatas = of(data);
+    });
+  }
+
+  handlError() {
+    console.log('error Capacitaor Motion not available');
+  }
 }
