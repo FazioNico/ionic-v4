9bb2eb9b37fd7ab1c1205ce4aed4f752d44898f6
FazioNico <contact@nicolasfazio.ch>
2018-06-13 09:34:31 +0200

    Update version [ci skip]

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-git add -f ./www
+git add -f ./MyApp/www
 git add --all
 git commit --allow-empty -m 'Update version [ci skip]'
-git subtree split -P www -b gh-pages
-git rm -r --cached ./www
+git subtree split -P /MyApp/www -b gh-pages
+git rm -r --cached ./MyApp/www
 git add --all
 git commit --allow-empty -m 'Deploy to Github Pages [ci skip]'
 git push origin gh-pages:gh-pages --force
